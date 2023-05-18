e3b2edbad8b4672a3794c4cf1ce1e1974b01899f
Wazed Biplob <www.biplob@gmail.com>
2023-05-18 17:43:58 +0600

    Fetching Category Info From MongoDB

--- index.js
+++ index.js
@@ -2,10 +2,50 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+//
+//
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+const data = require("./toyData.json");
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://unitoy:S3oiPff2aQtd33VV@cluster0.dp2hutp.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const toyCollection = client.db("unitoy").collection("toydata");
+    app.get("/toydata", async (req, res) => {
+      const cursor = toyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("UniToy Server Running.");
